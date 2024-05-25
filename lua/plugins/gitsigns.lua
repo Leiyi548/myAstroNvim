20471e823204d47dc6a960ba18da13c12d9e960b
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-05-25 22:24:15 +0800

    添加 gitsigns 插件配置

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,46 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = "User AstroGitFile",
+  opts = {
+    signs = {
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
+    },
+    numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+    preview_config = {
+      -- Options passed to nvim_open_win
+      border = "solid",
+      style = "minimal",
+      relative = "cursor",
+      row = 0,
+      col = 1,
+    },
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    on_attach = function(buffer)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, desc) vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc }) end
+        -- stylua: ignore start
+        map("n", "]g", gs.next_hunk, "Next Hunk")
+        map("n", "[g", gs.prev_hunk, "Prev Hunk")
+        map("n", "<leader>gj", gs.next_hunk, "Next Hunk")
+        map("n", "<leader>gk", gs.prev_hunk, "Prev Hunk")
+        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+        map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+        map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+        map( "n", "<leader>gp", gs.preview_hunk, "Preview the hunk at the cursor position in a floating window.")
+        map("n", "<leader>gP", gs.preview_hunk_inline, "Preview Hunk Inline")
+        map("n", "<leader>gd", gs.diffthis, "Diff current buffer(use <leader>bD to close)")
+        map("n", "<leader>ghb", function() gs.blame_line({ full = true }) end, "Blame Line")
+        map("n", "<leader>ghd", gs.diffthis, "Diff This")
+        map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+    end,
+  },
+}
