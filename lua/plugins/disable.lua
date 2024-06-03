e79a799494e6ccff9bd26c4397358a4630bfe281
leiyi548 <76139422+Leiyi548@users.noreply.github.com>
2024-06-04 02:23:55 +0800

    disable resession plugin

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,10 +1,5 @@
 return {
-  {
-    "goolord/alpha-nvim",
-    enabled = false
-  },
-  {
-    "max397574/better-escape.nvim",
-    enabled = false
-  }
+  { "goolord/alpha-nvim", enabled = false },
+  { "max397574/better-escape.nvim", enabled = false },
+  { "stevearc/resession.nvim", enabled = false },
 }
